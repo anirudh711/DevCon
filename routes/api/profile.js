e78e2ed330622f101b3cc030584f7cf40d4e7cc8
Anirudh Madhavan <anirudhragavender@gmail.com>
2020-08-19 00:07:17 +0530

    profile init

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,51 @@
 const express=require('express');
 const router=express.Router();
-
-//@route  GET api/profile
-//@desc   Test route 
-//@access Public
-router.get('/',async (req,res)=>{
-    res.send("profile route")
+const auth=require('../../middleware/auth');
+const {
+    check,
+    vaidationResult,
+    validationResult,
+  } = require("express-validator");
+//models
+const Profile=require('../../models/Profile');
+const User=require('../../models/Users');
+//@route  GET api/profile/me
+//@desc   Get current user's profile
+//@access Private
+router.get('/me',auth,async (req,res)=>{
+    try{
+        const profile=await Profile.findOne({user:req.user.id}).populate(
+            'user',
+            ['name','avatar']
+        );
+        if(!profile){
+            return res.status(400).json({msg:'There is no profile for this user'});
+        }
+        res.json(profile);
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 })
+//@route  POST api/profile
+//@desc   Get current user's profile
+//@access Private
+router.post('/',[auth,[
+    check('status',
+    'Status is required')
+    .not()
+    .isEmpty(),
+    check('skills',
+    'Skills is required').
+    not().
+    isEmpty()
+ ]],async(req,res)=>{
+     const errors=validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()})
+    }
+
+})
+
 
 module.exports=router;
