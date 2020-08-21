56fc183fd962879a5a9b895e1b118a05e2000caf
Anirudh Madhavan <anirudhragavender@gmail.com>
2020-08-21 16:15:00 +0530

    posts crud

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const request=require('request');
+const config=require('config')
 const auth = require("../../middleware/auth");
 const {
   check,
@@ -271,5 +273,32 @@ res.json(profile);
 console.error(err.message);
 res.status(500).send('Server Error');
 }
+})
+//@route  GET api/profile/github/:username
+//@desc   get user repos from github
+//@access Public
+router.get('/github/:username',(req,res)=>{
+  try {
+    
+    const options={
+      uri:`https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('gitID')}&client_secret=${config.get('gitSec')}`,
+      method:'GET',
+      headers:{'user-agent':'node.js'}
+    }
+      request(options,(error,response,body)=>{
+        if(error) 
+          console.error(error);
+       
+        if(response.statusCode!==200){
+          return res.status(404).json({msg:'No github profile found'})
+        }
+        
+          res.json(JSON.parse(body)) 
+      })
+     
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send('Server error')
+  }
 })
 module.exports = router;
